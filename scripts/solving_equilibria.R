1f26e0336714871ce8f36ba60f8ab91d44a1cf04
Walsh <bwalsh@liverpool.ac.uk>
2019-07-17 16:29:49 +0100

    Final commit of the day

--- scripts/solving_equilibria.R
+++ scripts/solving_equilibria.R
@@ -1,12 +1,19 @@
-
+rm(list = ls())
+# Loading packages ####
+require(ggplot2)
+require(reshape2)
+require(raster)
+require(maptools)
+require(scales)
+require("binom")
+require("dplyr")
+require("lazyeval")
 
 
 logistic_solv<- function(r, mf){
-  
-  
-  ff<- (d*(r+1/mf))/(b*r) # Basic equation
-  output<- c(ff)          # Stores y value in vector
-  return(output)         # Returns y value as output
+  ff<- (d*(r+1/mf))/(b*r)  # Basic equation
+  output<- c(ff)           # Stores ff value in vector
+  return(output)           # Returns ff value as output
 }
 
 b = 50
@@ -24,20 +31,27 @@ for(i in 1:length(mf_array)){                                     # For every it
   print(i)
   for(k in 1:length(r_array)){                                    # and every iteration of mating rate,
     mf<-mf_array[i]                                               # mf becomes i'th value of mf_array
-    r<-r_array[k]                                                 # x becomes k'th value of r_array
-    logistic_fun<- data.frame(logistic_solv(mf, r))  # calculate function and put in data frame
-    logistic_fun <- cbind(logistic_fun, mf, r)                # bind together y, mf, and x
-    logistic_all <- rbind(logistic_all,logistic_fun)      # bind together each iteration of data
+    r<-r_array[k]                                                 # r becomes r'th value of r_array
+    logistic_fun<- data.frame(logistic_solv(mf, r))               # calculate function and put in data frame
+    logistic_fun <- cbind(logistic_fun, mf, r)                    # bind together ff, mf, and r
+    logistic_all <- rbind(logistic_all,logistic_fun)              # bind together each iteration of data
   }
 }
+
+colnames(logistic_all)<- c("ff", "mf", "r")
+logistic_all$r <- factor(logistic_all$r, levels = c('15', '5', '3', '2', '1.5', '1'))
+
+
 
 library(RColorBrewer)
 cols<- brewer.pal(7,"RdPu")
 
-colnames(logistic_all)<- c("ff", "mf", "r")
-solv_extinction <- ggplot(logistic_all, aes( x= mf, y=ff, colour=factor(r)))+
-                          geom_line(size=1) +
-                          scale_color_manual("  r", values=cols[7:2])+
+
+#Plotting extinction zone ####
+
+solv_extinction <- ggplot(logistic_all, aes( x= mf, y=ff, fill=factor(r)))+
+                          geom_polygon( aes(x= mf, y=ff, group = factor(r))) +
+                          scale_fill_manual("  r", values= cols[2:7], guide = guide_legend(reverse=TRUE))+
                           coord_cartesian(xlim = c(0,1), ylim = c(0,1))+
                           scale_x_continuous(breaks = seq(0, 1, by=0.2),expand= c(0,0))+
                           scale_y_continuous(breaks = seq(0, 1, by=0.2),expand= c(0,0))+
@@ -52,7 +66,9 @@ solv_extinction <- ggplot(logistic_all, aes( x= mf, y=ff, colour=factor(r)))+
                                 panel.grid.major = element_blank(),
                                 panel.grid.minor = element_blank(),
                                 panel.background = element_blank())
+solv_extinction
 
-png(filename="figures/stability_extinction.png", type="cairo", units="in", width=6.9, height=6, pointsize=12, res=2000)
-print(solv_extinction)
-dev.off()
+
+#png(filename="figures/stability_extinction.png", type="cairo", units="in", width=6.9, height=6, pointsize=12, res=2000)
+#print(solv_extinction)
+#dev.off()
